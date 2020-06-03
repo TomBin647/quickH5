bc874aa3134258831d8e6ce349c9746f0fc178cf
Gaobin <gaobin@is-cool.com>
2020-06-03 13:14:52 +0800

    Initial Commit

---
+++ quickH5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quickH5
+//
+//  Created by Gaobin on 2020/6/3.
+//  Copyright © 2020 Gaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ quickH5/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  quickH5
+//
+//  Created by Gaobin on 2020/6/3.
+//  Copyright © 2020 Gaobin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
